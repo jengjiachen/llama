4d36ee51f85f8e5e7cf1c2957524e77cb1207da9
Jiajia Chen <chenjengjia@gmail.com>
2020-02-13 00:21:16 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,20 @@
-function setup () {
-    createCanvas(600, 600);
-}
 
-function popArt(originX, originY, outlineColor, bgColor, wingsColor, centerColor) {
-    translate(originX, originY); 
-    fill(bgColor);
-    noStroke();
-    rect(0, 0, 200, 200);
-    
-    stroke(outlineColor);
-    strokeWeight(15);
-    line(80, 0, 200, 100);
-    line(200, 100, 80, 200);
-    fill(wingsColor);
-    
-    arc(200, 100, 180, 180, HALF_PI, PI + HALF_PI);
-    arc(0, 100, 180, 180, PI + HALF_PI, HALF_PI);
-    
-    fill (centerColor);
-    circle(100, 100, 90);
-    
-}
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
-function draw() {
-    popArt(0,0,'#07485C', '#567987','#F4857F','#F9C0AD');
-    popArt(0,200, '#655A8C','#D8C5D9','#BF8FBC','#B099BF');
-    popArt(0,200, '#07485C', '#567987','#F4857F','#F9C0AD');
-    
-    popArt(200,-400, '#655A8C','#D8C5D9','#BF8FBC','#B099BF');
-    popArt(0,200,'#07485C', '#567987','#F4857F','#F9C0AD');
-    popArt(0,200, '#655A8C','#D8C5D9','#BF8FBC','#B099BF');
-    
-    popArt(200,-400,'#07485C', '#567987','#F4857F','#F9C0AD');
-    popArt(0,200, '#655A8C','#D8C5D9','#BF8FBC','#B099BF');
-    popArt(0,200, '#07485C', '#567987','#F4857F','#F9C0AD');
 
+const heading = document.createElement('h1');
+heading.innerHTML = 'Llamas don\'t bite. They spit when they\'re agitated, but mostly at each other.';
+
+
+heading.style.cursor = 'pointer';
+
+
+heading.addEventListener('click', handleHeadingClick);
+
+
+myEmptyDiv.appendChild(heading);
+
+ 
+function handleHeadingClick (){
+    document.body.style.backgroundColor = 'pink';
 }
